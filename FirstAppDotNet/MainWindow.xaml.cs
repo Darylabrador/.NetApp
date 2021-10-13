b566493431168d2da401cc4399772f11ecc77b8e
MatthiasEntropik <matthias@entropik.digital>
2021-10-13 16:58:45 +0400

    pause play musique

--- FirstAppDotNet/MainWindow.xaml.cs
+++ FirstAppDotNet/MainWindow.xaml.cs
@@ -1,90 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
 using System.Windows;
-
-
 namespace FirstAppDotNet
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public List<FileClass> dataList { get; set; }
-		public List<FileClass> list = new List<FileClass>();
-		public MainWindow()
-		{
-			InitializeComponent();
-			getFiles();
-			LoadListItems();
-		}
-		private void LoadListItems()
-		{
-			dataList = new List<FileClass>();
-			dataList = list;
-			ListBoxFiles.ItemsSource = dataList;
-			this.ListBoxFiles.DataContext = this;
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<FileClass> dataList { get; set; }
+        public List<FileClass> list = new List<FileClass>();
+        public FileClass FileInfo = new FileClass();
+        public bool isPlaying = false;
+        WMPLib.WindowsMediaPlayer wmplayer = new WMPLib.WindowsMediaPlayer();
 
-		private void getFiles()
-		{
-			string[] drives = Environment.GetLogicalDrives();
+        public MainWindow()
+        {
+            InitializeComponent();
+            getFiles();
+            LoadListItems();
+        }
+        private void LoadListItems()
+        {
+            dataList = new List<FileClass>();
+            dataList = list;
+            ListBoxFiles.ItemsSource = dataList;
+            this.ListBoxFiles.DataContext = this;
+        }
 
-			foreach (string dr in drives)
-			{
-				DriveInfo di = new DriveInfo(dr);
-				DirectoryInfo rootDir = di.RootDirectory;
-				WalkDirectoryTree(rootDir);
-			}
-		}
-		void WalkDirectoryTree(DirectoryInfo root)
-		{
-			DirectoryInfo[] subDirs;
+        private void getFiles()
+        {
+            string[] drives = Environment.GetLogicalDrives();
 
-			FileInfo[] files = getMp3(root);
+            foreach (string dr in drives)
+            {
+                DriveInfo di = new DriveInfo(dr);
+                DirectoryInfo rootDir = di.RootDirectory;
+                WalkDirectoryTree(rootDir);
+            }
+        }
+        void WalkDirectoryTree(DirectoryInfo root)
+        {
+            DirectoryInfo[] subDirs;
 
-			if (files!=null)
-			{
-				foreach (FileInfo fi in files)
-				{
-					FileClass vItem;
-					vItem = new FileClass();
-					vItem.Name = "Name";
-					vItem.PicturePath = "Images/mp3.png";
-					vItem.Path = fi.FullName;
-					list.Add(vItem);
-				}
+            FileInfo[] files = getMp3(root);
+            if (files != null)
+            {
+                foreach (FileInfo file in files)
+                {
+                    createFileClass(file);
+                }
 
-				subDirs = root.GetDirectories();
+                subDirs = root.GetDirectories();
 
-				foreach (DirectoryInfo dirInfo in subDirs)
-				{
-					WalkDirectoryTree(dirInfo);
-				}
-			}
-		}
-		FileInfo[] getMp3(DirectoryInfo root)
-		{
-			FileInfo[] files = null;
-			try
-			{
-				files = root.GetFiles("*.mp3");
-			}
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo);
+                }
+            }
+        }
+        private void createFileClass(FileInfo file)
+        {
+            FileClass tempItem;
+            tempItem = new FileClass();
+            tempItem.Name = file.Name;
+            tempItem.PicturePath = "Images/mp3.png";
+            tempItem.Path = file.FullName;
+            list.Add(tempItem);
+        }
+        FileInfo[] getMp3(DirectoryInfo root)
+        {
+            FileInfo[] files = null;
+            try
+            {
+                files = root.GetFiles("*.mp3");
+            }
 
-			catch (UnauthorizedAccessException e)
-			{
+            catch (UnauthorizedAccessException e)
+            {
 
-				Console.WriteLine(e.Message);
-			}
+                Console.WriteLine(e.Message);
+            }
 
-			catch (DirectoryNotFoundException e)
-			{
-				Console.WriteLine(e.Message);
-			}
-			return files;
-		}
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return files;
+        }
+        private void playMusic(object sender, EventArgs e)
+        {
+            wmplayer.controls.stop();
+            wmplayer.URL = list[ListBoxFiles.SelectedIndex].Path;
+            wmplayer.controls.play();
+            isPlaying = true;
+        }
 
-		}
+        private void musicCommand(object sender, EventArgs e)
+        {
+            if(isPlaying == true)
+            {
+                wmplayer.controls.pause();
+                isPlaying = true;
+            }
+            else
+            {
+                wmplayer.controls.play();
+                isPlaying = false;
+            }
+        }
+        private void stopCommand(object sender, EventArgs e)
+        {
+            wmplayer.controls.stop();
+        }
+    }
 }
